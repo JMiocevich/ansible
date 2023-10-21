233e3e2a2565a4cfefa3af8dcdf09042ca778d93
Joseph Miocevich <joseph.miocevich@mechanicalrock.io>
2023-10-21 15:38:34 +0800

    -

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install ansible
 
 

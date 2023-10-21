47755927dcd4221f673ff876d95942a4cafd3847
Joseph Miocevich <jmiocevich@gmail.com>
2023-10-21 18:41:44 +0800

    -

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# Check if Homebrew is installed, install if we don't have it
-if test ! $(which brew); then
-    echo "Installing Homebrew..."
+# Check if Homebrew is installed, if not, install it
+if ! command -v brew &>/dev/null; then
+    echo "Homebrew not found. Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo "Homebrew is already installed."
 fi
 
-# Update Homebrew repositories and formulae
-echo "Updating Homebrew..."
+# Update Homebrew recipes
 brew update
 
 # Install Ansible
 echo "Installing Ansible..."
 brew install ansible
 
-echo "Ansible installation completed!"
+# Verify installation
+if command -v ansible &>/dev/null; then
+    echo "Ansible successfully installed!"
+    ansible --version
+else
+    echo "Something went wrong with Ansible installation. Please check the installation logs."
+fi

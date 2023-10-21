5952814a25e2ce907eb5afd99ccd72e3477e2347
Joseph Miocevich <joseph.miocevich@mechanicalrock.io>
2023-10-21 16:15:52 +0800

    -

--- install.sh
+++ install.sh
@@ -5,11 +5,6 @@ set -e
 
 # Shared functions
 
-pretty_print() {
-  printf "\n%b\n" "$1"
-}
-# 
-pretty_print "Here we go..."
 
 # So it begins
 
@@ -17,23 +12,23 @@ pretty_print "Here we go..."
 # Homebrew installation
 
 if ! command -v brew &>/dev/null; then
-  pretty_print "Installing Homebrew, an OSX package manager, follow the instructions..." 
+  echo "Installing Homebrew, an OSX package manager, follow the instructions..." 
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 
   if ! grep -qs "recommended by brew doctor" ~/.zshrc; then
-    pretty_print "Put Homebrew location earlier in PATH ..."
+    echo "Put Homebrew location earlier in PATH ..."
       printf '\n# recommended by brew doctor\n' >> ~/.zshrc
       printf 'export PATH="/usr/local/bin:$PATH"\n' >> ~/.zshrc
       export PATH="/usr/local/bin:$PATH"
   fi
 else
-  pretty_print "You already have Homebrew installed...good job!"
+  echo "You already have Homebrew installed...good job!"
 fi
 
 # Homebrew OSX libraries
 
-pretty_print "Updating brew formulas"
+echo "Updating brew formulas"
   	brew update
 brew install ansible
 

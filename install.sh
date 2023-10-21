4d567eb2a0e3d9a420d9b4b62217d8aeb648ea71
Joseph Miocevich <joseph.miocevich@mechanicalrock.io>
2023-10-21 19:36:08 +0800

    update

--- install.sh
+++ install.sh
@@ -7,24 +7,26 @@ if [ "$EUID" -eq 0 ]; then
 fi
 
 # Check if Homebrew is already installed
-if command -v brew > /dev/null 2>&1; then
-    echo "Homebrew is already installed."
-    exit 0
-fi
+# if command -v brew > /dev/null 2>&1; then
+#     echo "Homebrew is already installed."
+#     exit 0  
+# fi
 
 # Install Homebrew
 echo "Installing Homebrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
 
-# Add Homebrew to PATH, if it's not already in there
-if [[ ! "$PATH" =~ (^|:)/usr/local/bin(:|$) ]]; then
-    echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
-    source ~/.zshrc
-fi
-
-echo "Homebrew installation complete."
-
 (echo; echo 'eval "$(opt/homebrew/bin/brew shellenv)"') >> .zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
+echo "Homebrew installation complete."
+
 brew install ansible 
+
+mkdir -p personal && cd personal
+
+git clone git@github.com:JMiocevich/ansible.git
+
+cd ansible
+
+ansible-playbook playbook.yml 

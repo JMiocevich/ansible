dcecc3ae57b373486a822504ee7a369d657b362c
Joseph Miocevich <jmiocevich@gmail.com>
2023-10-21 20:35:08 +0800

    fix

--- init.sh
+++ init.sh
@@ -26,7 +26,7 @@ if ! xcode-select -p &>/dev/null; then
 fi
 
 echo "Installing Homebrew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" </dev/null
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 (echo; echo 'eval "$(opt/homebrew/bin/brew shellenv)"') >> .zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"

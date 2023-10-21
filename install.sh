fb38006d9a6e683783f49911c1e4b8797689a37f
Joseph Miocevich <joseph.miocevich@mechanicalrock.io>
2023-10-21 16:16:27 +0800

    -

--- install.sh
+++ install.sh
@@ -18,8 +18,8 @@ if ! command -v brew &>/dev/null; then
 
   if ! grep -qs "recommended by brew doctor" ~/.zshrc; then
     echo "Put Homebrew location earlier in PATH ..."
-      printf '\n# recommended by brew doctor\n' >> ~/.zshrc
-      printf 'export PATH="/usr/local/bin:$PATH"\n' >> ~/.zshrc
+      echo '\n# recommended by brew doctor\n' >> ~/.zshrc
+      echo 'export PATH="/usr/local/bin:$PATH"\n' >> ~/.zshrc
       export PATH="/usr/local/bin:$PATH"
   fi
 else

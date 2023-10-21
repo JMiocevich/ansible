866287c99b2ebb4fd60df65f4acb124b7e5e6fdf
Joseph Miocevich <joseph.miocevich@mechanicalrock.io>
2023-10-21 15:29:26 +0800

    -

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 brew install ansible
+
+
+mkdir personal
+
+cd personal
+
+git clone git@github.com:JMiocevich/ansible.git
+cd ansible
 
 ansible-playbook playbook.yml

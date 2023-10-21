ba072d904c7269377b14b69353102298d906b2af
Joseph Miocevich <joseph.miocevich@mechanicalrock.io>
2023-10-21 16:20:08 +0800

    -

--- install.sh
+++ install.sh
@@ -1,42 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# This is part of a larger script for setting a mac for python development.
-set -e
-
-# Shared functions
-
-
-# So it begins
-
-
-# Homebrew installation
-
-if ! command -v brew &>/dev/null; then
-  echo "Installing Homebrew, an OSX package manager, follow the instructions..." 
+# Check if Homebrew is installed, install if we don't have it
+if test ! $(which brew); then
+    echo "Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-
-  if ! grep -qs "recommended by brew doctor" ~/.zshrc; then
-      echo 'export PATH="/usr/local/bin:$PATH"\n' >> ~/.zshrc
-      export PATH="/usr/local/bin:$PATH"
-  fi
-else
-  echo "You already have Homebrew installed...good job!"
 fi
 
-# Homebrew OSX libraries
-
-echo "Updating brew formulas"
+# Update Homebrew repositories and formulae
+echo "Updating Homebrew..."
 brew update
+
+# Install Ansible
+echo "Installing Ansible..."
 brew install ansible
 
-
-mkdir personal
-
-cd ./personal
-
-git clone git@github.com:JMiocevich/ansible.git
-cd ansible
-
-ansible-playbook playbook.yml
-
+echo "Ansible installation completed!"

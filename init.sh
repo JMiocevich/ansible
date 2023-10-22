5e4c6de72d54dce0d6039d9a17b88e35afdb3167
Joseph Miocevich <joseph.miocevich@mechanicalrock.io>
2023-10-22 17:09:04 +0800

    -

--- init.sh
+++ init.sh
@@ -1,40 +1,50 @@
-#!/bin/bash
+# #!/bin/bash
 
-# Check if the script is being run as root
-if [ "$EUID" -eq 0 ]; then
-    echo "Do not run this script as root."
-    exit 1
-fi
-
-# Check if Homebrew is already installed
-# if command -v brew > /dev/null 2>&1; then
-#     echo "Homebrew is already installed."
-#     exit 0  
+# # Check if the script is being run as root
+# if [ "$EUID" -eq 0 ]; then
+#     echo "Do not run this script as root."
+#     exit 1
 # fi
 
-# Install Homebrew
+# # Check if Homebrew is already installed
+# # if command -v brew > /dev/null 2>&1; then
+# #     echo "Homebrew is already installed."
+# #     exit 0  
+# # fi
 
-# Check for Xcode CLT and install if necessary
-if ! xcode-select -p &>/dev/null; then
-    echo "Installing Xcode Command Line Tools..."
-    xcode-select --install &>/dev/null
+# # Install Homebrew
 
-    # Wait until the Xcode CLT is installed
-    until xcode-select -p &>/dev/null; do
-        sleep 5
-    done
-fi
+# # Check for Xcode CLT and install if necessary
+# if ! xcode-select -p &>/dev/null; then
+#     echo "Installing Xcode Command Line Tools..."
+#     xcode-select --install &>/dev/null
 
-echo "Installing Homebrew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#     # Wait until the Xcode CLT is installed
+#     until xcode-select -p &>/dev/null; do
+#         sleep 5
+#     done
+# fi
 
-(echo; echo 'eval "$(opt/homebrew/bin/brew shellenv)"') >> .zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# echo "Installing Homebrew..."
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-echo "Homebrew installation complete."
+# (echo; echo 'eval "$(opt/homebrew/bin/brew shellenv)"') >> .zprofile
+# eval "$(/opt/homebrew/bin/brew shellenv)"
 
-brew install ansible 
+# echo "Homebrew installation complete."
 
+# brew install ansible 
+
+# mkdir -p $HOME/personal && cd $HOME/personal
+
+# git clone https://github.com/JMiocevich/ansible.git
+
+# cd ansible  
+
+# ansible-playbook playbook.yml 
+
+
+pip3 install ansible
 mkdir -p $HOME/personal && cd $HOME/personal
 
 git clone https://github.com/JMiocevich/ansible.git

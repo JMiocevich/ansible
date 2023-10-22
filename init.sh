47246239e3df2899cdfbe5df5a029032a3c0ba2a
Joseph Miocevich <joseph.miocevich@mechanicalrock.io>
2023-10-22 19:32:00 +0800

    move to ansible

--- init.sh
+++ init.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+
+
+# # Check for Xcode CLT and install if necessary
+# if ! xcode-select -p &>/dev/null; then
+#     echo "Installing Xcode Command Line Tools..."
+#     xcode-select --install &>/dev/null
+
+#     # Wait until the Xcode CLT is installed
+#     until xcode-select -p &>/dev/null; do
+#         sleep 5
+#     done
+# fi
+
+
+
 pip3 install ansible
 mkdir -p $HOME/personal && cd $HOME/personal
 

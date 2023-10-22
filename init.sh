544394e6ebfd86e03a08cd8fab8ba094060dac38
Joseph Miocevich <joseph.miocevich@mechanicalrock.io>
2023-10-22 19:54:03 +0800

    xcode update

--- init.sh
+++ init.sh
@@ -2,20 +2,14 @@
 
 
 # # Check for Xcode CLT and install if necessary
-
-# Check if Xcode Command Line Tools are installed
-if xcode-select -p &>/dev/null; then
-    echo "Xcode Command Line Tools are already installed!"
-else
-    # Prompt to install the Xcode Command Line Tools
+if ! xcode-select -p &>/dev/null; then
     echo "Installing Xcode Command Line Tools..."
-    xcode-select --install
+    xcode-select --install &>/dev/null
 
-    # Wait until the installation is finished
-    echo "Please wait for the installation to finish, then press any key to continue..."
-    read -n 1 -s
-
-    echo "Installation done!"
+    # Wait until the Xcode CLT is installed
+    until xcode-select -p &>/dev/null; do
+        sleep 5
+    done
 fi
 
 

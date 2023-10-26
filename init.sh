018eb2eb485b0fb655996eafcff8376adffa5b64
Joseph Miocevich <joseph.miocevich@mechanicalrock.io>
2023-10-26 16:32:27 +1100

    update

--- init.sh
+++ init.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
-
-
-# # Check for Xcode CLT and install if necessary
-if ! xcode-select -p &>/dev/null; then
-    echo "Installing Xcode Command Line Tools..."
-    echo "Click install on pop-up window"
-    xcode-select --install &>/dev/null
-
-    # Wait until the Xcode CLT is installed
-    until xcode-select -p &>/dev/null; do
-        sleep 5
-    done
-fi
-
-
-
-pip3 install ansible
-
-echo 'export PATH="/Users/joe/Library/Python/3.9/bin:$PATH"' >> ~/.zshrc
-source ~/.zshrc
-
+brew install ansible
 
 mkdir -p $HOME/personal && cd $HOME/personal
 
